66da01d507f7fe9105eb2020eb8e2cb7013327bc
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-03-27 20:24:27 +0900

    Support named argument

--- src/Router.php
+++ src/Router.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace BEAR\AutoRouter;
 
 use AutoRoute\Router as Origin;
+use ReflectionParameter;
 
 use function array_shift;
 use function reset;
@@ -30,5 +31,42 @@ class Router extends Origin
         $this->subNamespace = $subNamespace;
         array_shift($this->segments);
         $this->captureMainClass();
+    }
+
+    protected function captureArgument(
+        ReflectionParameter $parameter,
+        int $i
+    ): void {
+        if (empty($this->segments)) {
+            return;
+        }
+
+        if ($parameter->isVariadic()) {
+            $this->captureVariadic($parameter, $i);
+
+            return;
+        }
+
+        $this->arguments[$parameter->name] = $this->filter->parameter(
+            $parameter,
+            $this->segments
+        );
+        $name = $parameter->getName();
+        $this->log("captured argument {$i} (\${$name})");
+    }
+
+    protected function captureVariadic(
+        ReflectionParameter $parameter,
+        int $i
+    ): void {
+        $name = $parameter->getName();
+
+        while (! empty($this->segments)) {
+            $this->arguments[$parameter->name] = $this->filter->parameter(
+                $parameter,
+                $this->segments
+            );
+            $this->log("captured variadic argument {$i} (\${$name})");
+        }
     }
 }
